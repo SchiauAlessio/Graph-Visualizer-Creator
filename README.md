be2c18a268f34d30e9794ac002181dc9179c292a
SchiauAlessio <alessio.schiau1@gmail.com>
2024-06-03 18:45:30 +0300

    graph editor

--- README.md
+++ README.md
@@ -1,1 +1,35 @@
-# Graph-Visualizer-Creator
+# Angular Graph Editor
+
+This web application is an Angular project that allows users to create, edit, and visualize graphs using Graphviz. It provides an intuitive interface for editing graph code and dynamically rendering the graph visualization.
+
+## Features
+
+- **Graph Editing**: Users can enter graph code directly into the editor textarea. The code is then parsed and rendered as a graph visualization.
+- **Graph Rendering**: The application uses D3.js and Graphviz to render the graph visualization in real-time based on the input code.
+- **Sample Graphs**: Users can load pre-defined sample graphs to experiment with different graph structures and layouts.
+- **Download Graph**: Users can download the rendered graph as either a JPG or PNG image file.
+- **Engine Selection**: Users can choose the rendering engine for Graphviz, allowing for different graph layout algorithms.
+
+
+### Graph Editor Component
+
+- Contains the main functionality of the application, including the graph editor, viewer, and menu.
+- Renders a textarea for entering graph code.
+- Dynamically updates the graph visualization based on the input code.
+- Provides options for loading sample graphs, downloading the graph, and selecting the rendering engine.
+- Utilizes D3.js and Graphviz for rendering the graph visualization.
+- Implements platform detection to ensure certain operations (e.g., downloading the graph) only occur in the browser environment.
+
+## Usage
+
+To run the Angular Graph Editor:
+
+1. Clone the repository.
+2. Install dependencies using npm or yarn.
+3. Run the application using the Angular CLI (`ng serve`).
+
+## License
+
+This project is licensed under the [MIT License](LICENSE).
+
+For more information, please refer to the [Graphviz Documentation](https://graphviz.org/documentation/) or [report any issues](https://github.com/ellson/graphviz/issues).
--- frontend/src/app/graph-render/graph-editor/graph-editor.component.ts
+++ frontend/src/app/graph-render/graph-editor/graph-editor.component.ts
@@ -1,7 +1,6 @@
 import {Component, Inject, PLATFORM_ID} from '@angular/core';
 import {FormsModule} from "@angular/forms";
 import {Engine, graphviz} from 'd3-graphviz';
-import { Graphviz } from 'd3-graphviz';
 import {isPlatformBrowser, NgForOf} from "@angular/common";
 import {samples} from "../util/samples";
 
@@ -25,29 +24,73 @@ export class GraphEditorComponent {
   constructor(@Inject(PLATFORM_ID) private platformId: any) {}
 
   ngOnInit(): void {
+
     this.renderGraph();
   }
 
   renderGraph(): void {
+
     if (isPlatformBrowser(this.platformId)) {
       const graph = document.getElementById('graph');
       if (graph) {
         graphviz(graph).engine(this.engine).renderDot(this.graphvizCode);
       }
     }
+  }
 
+  downloadGraph(format: string): void {
+
+    if (isPlatformBrowser(this.platformId)) {
+
+      const graph = document.getElementById('graph');
+      if (graph) {
+
+        const svgElement: SVGSVGElement | null = graph.querySelector('svg');
+        if (svgElement) {
+
+          const svgString = new XMLSerializer().serializeToString(svgElement);
+          const canvas: HTMLCanvasElement = document.createElement('canvas');
+          const ctx: CanvasRenderingContext2D | null = canvas.getContext('2d');
+          const img: HTMLImageElement = new Image();
+
+          img.onload = () => {
+
+            canvas.width = img.width;
+            canvas.height = img.height;
+            ctx?.drawImage(img, 0, 0);
+            canvas.toBlob(blob => {
+
+              if (blob) {
+
+                const url = URL.createObjectURL(blob);
+                const a = document.createElement('a');
+                a.href = url;
+                a.download = `graph.${format}`;
+                a.click();
+                URL.revokeObjectURL(url);
+              }
+            }, `image/${format}`);
+          };
+
+          img.src = 'data:image/svg+xml;base64,' + btoa(svgString);
+        }
+      }
+    }
   }
 
   sampleKeys(): string[] {
+
     return Object.keys(this.samples);
   }
 
   loadSample(key: string): void {
+
     this.graphvizCode = this.samples[key];
     this.renderGraph();
   }
 
   setEngine(engine: Engine): void {
+
     this.engine = engine;
     this.renderGraph();
   }
